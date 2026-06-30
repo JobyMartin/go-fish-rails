661da41adab2f7447aef5da8aa9930ed554e472a
Joby Martin <jobymartin4th@gmail.com>
2026-06-30 18:23:17 -0400

    Add email and password validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  validates :email_address, presence: true, uniqueness: { case_insensitive: true }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_secure_password
+  validates :password, length: { minimum: 8 }, allow_nil: true
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when bad email is provided' do
+    let(:user) { build(:user, email_address: 'toast') }
+    it 'is invalid' do
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'when good email is provided' do
+    let(:user) { build(:user, email_address: 'toast@grilledcheese.com') }
+    it 'is valid' do
+      expect(user).to be_valid
+    end
+  end
+
+  context 'when bad password is provided' do
+    let(:user) { build(:user, password: 'toast') }
+    it 'is valid' do
+      expect(user).to be_invalid
+    end
+  end
+
+  context 'when good password is provided' do
+    let(:user) { build(:user, password: 'grilledcheese') }
+    it 'is valid' do
+      expect(user).to be_valid
+    end
+  end
 end
