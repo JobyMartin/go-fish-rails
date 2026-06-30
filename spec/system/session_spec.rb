84950794e19ca76983d98bc8db65ccd228d9a56c
Joby Martin <jobymartin4th@gmail.com>
2026-06-30 14:05:43 -0400

    Add spec for forgot password flow

--- spec/system/session_spec.rb
+++ spec/system/session_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'Session', type: :system do
+RSpec.fdescribe 'Session', type: :system do
   let(:user) { create(:user) }
-
-  before do
-    sign_in(user)
-  end
 
   it 'shows the login page' do
     visit new_session_path
     expect(page).to have_content 'Email'
     expect(page).to have_content 'Password'
+  end
+
+  context 'when they click forgot password' do
+    before do
+      visit new_session_path
+      click_on 'Forgot password?'
+    end
+
+    it 'redirects to the new password page' do
+      expect(page).to have_current_path new_password_path
+      expect(page).to have_content 'Forgot your password?'
+    end
   end
 end
