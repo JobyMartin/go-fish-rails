30d19d99562cad305a22f08afc5f184c396e1acf
Joby Martin <jobymartin4th@gmail.com>
2026-07-02 12:47:26 -0400

    Add error message constant to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,8 @@
 class Player < ApplicationRecord
+  JOINED_ERROR_MESSAGE = 'You already joined the game'.freeze
+
   belongs_to :game
   belongs_to :user
 
-  validates :game_id, uniqueness: { scope: :user_id, message: 'You already joined the game' }
+  validates :game_id, uniqueness: { scope: :user_id, message: JOINED_ERROR_MESSAGE }
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
   let(:game) { create :game }
   let(:user) { create :user }
-  let(:error_message) { 'You already joined the game' }
   it 'allows a player to join only once' do
     valid_player = build(:player, game:, user:)
     expect(valid_player).to be_valid
@@ -11,6 +10,6 @@ RSpec.describe Player, type: :model do
 
     invalid_player = build(:player, game:, user:)
     expect(invalid_player).to_not be_valid
-    expect(invalid_player.errors.full_messages.to_sentence).to include(error_message)
+    expect(invalid_player.errors.full_messages.to_sentence).to include(Player::JOINED_ERROR_MESSAGE)
   end
 end
