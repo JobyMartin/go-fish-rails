47bd1e7b06af0a7b463a3c2d172ee35328e30668
Joby Martin <jobymartin4th@gmail.com>
2026-07-02 12:01:17 -0400

    Generate simple Player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_02_134157) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_02_155950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_02_134157) do
     t.integer "game_type", null: false
     t.string "name", null: false
     t.datetime "started_at"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
