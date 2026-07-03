e6a391507f8f1b96b3c82cba90ef4654b53562bf
Joby Martin <jobymartin4th@gmail.com>
2026-07-03 13:07:52 -0400

    Build simple games factory bot

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :game do
     name { 'My game' }
     game_type { 'go_fish' }
+
+    trait :in_progress do
+      started_at { 1.day.ago }
+    end
+
+    trait :ended do
+      started_at { 2.days.ago }
+      ended_at { 1.day.ago }
+    end
   end
 end
