b5fe8e341fb9a590f8947986de1424b008a51d17
Joby Martin <jobymartin4th@gmail.com>
2026-07-03 21:47:40 -0400

    Add dynamic rendering of stats to the stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,3 +1,17 @@
 
 class StatsController < ApplicationController
+  def index
+    @user = Current.session.user
+    @win_percentage = win_percentage
+    @games_won = winner_count
+  end
+
+  private
+
+  def win_percentage
+    "#{((winner_count / player_count) * 100).to_i}%"
+  end
+
+  def player_count = @user.players.count.to_f
+  def winner_count = @user.players.count { it.winner }
 end
--- spec/system/stats_spec.rb
+++ spec/system/stats_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Stats', type: :system do
   let(:user) { create(:user) }
+  let(:game1) { create(:game) }
+  let(:game2) { create(:game) }
 
   before do
     sign_in(user)
@@ -10,5 +12,32 @@ RSpec.describe 'Stats', type: :system do
   it 'shows the stats' do
     visit stats_path
     expect(page).to have_content 'Your Go Fish Statistics'
+  end
+
+  context 'when the user has played games' do
+    let!(:player) { create(:player, user: user, game: game1) }
+    let!(:player1) { create(:player, user: user, game: game2) }
+
+    it 'displays their total games' do
+      num_of_games = 2
+      visit stats_path
+      expect(page).to have_content num_of_games
+    end
+
+    it 'displays theit total games won' do
+      player.winner = true
+      player.save
+      num_of_wins = 1
+      visit stats_path
+      expect(page).to have_content num_of_wins
+    end
+
+    it 'displays theit win percentage' do
+      player.winner = true
+      player.save
+      win_percentage = '50%'
+      visit stats_path
+      expect(page).to have_content win_percentage
+    end
   end
 end
