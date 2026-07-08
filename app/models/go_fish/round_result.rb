f81a2951e1385e7bd00c76ae6301745a0fdef158
Joby Martin <jobymartin4th@gmail.com>
2026-07-08 08:18:45 -0400

    Port round result from sinatra app

---
+++ app/models/go_fish/round_result.rb
@@ -0,0 +1,37 @@
+module GoFish
+  class RoundResult
+    attr_reader :user_taken_from, :user_given_to, :cards_exchanged, :rank_in_question
+    attr_accessor :went_fishing, :made_a_catch
+
+    def initialize(current_user:, cards_exchanged:, user_in_question:, rank_in_question:, went_fishing:, made_a_catch:)
+      @user_taken_from = user_in_question
+      @rank_in_question = rank_in_question
+      @user_given_to = current_user
+      @cards_exchanged = cards_exchanged
+      @went_fishing = went_fishing
+      @made_a_catch = made_a_catch
+    end
+
+    def for_current_player
+      message = ["You asked #{user_taken_from.name} for any #{rank_in_question}s"]
+
+      message << "\nGo fish!" if went_fishing
+      message << "\nYou made a catch!" if made_a_catch
+
+      message << "\nYou took #{cards_exchanged.map(&:to_s).join(', ')} from #{user_taken_from.name}" unless went_fishing || made_a_catch
+
+      message
+    end
+
+    def for_other_players
+      message = ["#{user_given_to.name} asked #{user_taken_from.name} for any #{rank_in_question}s"]
+
+      message << "\n#{user_given_to.name} went fishing!" if went_fishing
+      message << "\n#{user_given_to.name} made a catch!" if made_a_catch
+
+      message << "\n#{user_given_to.name} took #{cards_exchanged.map(&:to_s).join(', ')} from #{user_taken_from.name}" unless went_fishing || made_a_catch 
+
+      message
+    end
+  end
+end
