cb3c8b640c772a9a99ebed79a411b8961238adf2
Joby Martin <jobymartin4th@gmail.com>
2026-07-08 11:48:14 -0400

    Add card suit default for ease in testing

--- app/models/go_fish/card.rb
+++ app/models/go_fish/card.rb
@@ -10,7 +10,7 @@ module GoFish
     RANKS = %w( 2 3 4 5 6 7 8 9 10 J Q K A )
     SUITS = %w( Diamonds Hearts Spades Clubs )
     
-    def initialize(rank, suit)
+    def initialize(rank, suit = 'Spades')
       raise InvalidRank unless RANKS.include? rank
       raise InvalidSuit unless SUITS.include? suit
       @rank = rank
