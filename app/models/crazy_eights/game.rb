2dde545b70b3d90b522e884fddacf54720812137
Joby Martin <jobymartin4th@gmail.com>
2026-07-10 09:28:36 -0400

    Add deal method to crazy eights game

--- app/models/crazy_eights/game.rb
+++ app/models/crazy_eights/game.rb
@@ -1,7 +1,14 @@
 module CrazyEights
   class Game
-    def initialize(players)
+    BIG_GAME_PLAYER_COUNT = 3
+    BIG_GAME_DEAL_COUNT = 5
+    SMALL_GAME_DEAL_COUNT = 7
+
+    attr_accessor :players, :deck
+
+    def initialize(players, deck = Deck.new)
       @players = players
+      @deck = deck
     end
     def self.load(json)
 
@@ -9,6 +16,20 @@ module CrazyEights
 
     def self.dump(obj)
       obj.as_json
+    end
+
+    def deal!
+      number_of_cards.times do
+        players.each do
+          it.add_cards([deck.top_card])
+        end
+      end
+    end
+
+    private
+
+    def number_of_cards
+      players.count < BIG_GAME_PLAYER_COUNT ? SMALL_GAME_DEAL_COUNT : BIG_GAME_DEAL_COUNT
     end
   end
 end
---
+++ spec/models/crazy_eights/game_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe CrazyEights::Game, type: :model do
+  let(:num_players) { 5 }
+  let!(:players) { Array.new(num_players) { |id| CrazyEights::Player.new(id) } }
+  let!(:crazy_eights_game) { described_class.new(players) }
+
+  describe '#deal!' do
+    let(:small_game_hand) { 5 }
+    let(:large_game_hand) { 7 }
+    context 'when there are more than 2 players' do
+      it 'deals the players 5 cards' do
+        crazy_eights_game.deal!
+        dealt_players_hands = crazy_eights_game.players.map(&:hand)
+
+        expect(dealt_players_hands.first.count).to eq small_game_hand
+        dealt_players_hands.first.each do
+          expect(it).to be_a CrazyEights::Card
+        end
+      end
+    end
+
+    context 'when there are 2 players or less' do
+      let(:num_players) { 2 }
+      let!(:players) { Array.new(num_players) { |id| CrazyEights::Player.new(id) } }
+      let!(:crazy_eights_game) { described_class.new(players) }
+
+      it 'deals the players 7 cards' do
+        crazy_eights_game.deal!
+        dealt_players_hands = crazy_eights_game.players.map(&:hand)
+
+        expect(dealt_players_hands.first.count).to eq large_game_hand
+        dealt_players_hands.first.each do
+          expect(it).to be_a CrazyEights::Card
+        end
+      end
+    end
+  end
+end
