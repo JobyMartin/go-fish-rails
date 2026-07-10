f32438a5aaf2641faf3ec243405c2d0bccaa8613
Joby Martin <jobymartin4th@gmail.com>
2026-07-10 11:13:45 -0400

    Add serialization to crazy eights player

--- app/models/crazy_eights/player.rb
+++ app/models/crazy_eights/player.rb
@@ -1,6 +1,6 @@
 module CrazyEights
   class Player
-
+    attr_reader :id
     attr_accessor :hand
 
     def initialize(id, hand = [])
@@ -10,6 +10,11 @@ module CrazyEights
 
     def add_cards(cards)
       cards.each { |card| hand << card }
+    end
+
+    def self.load(hash)
+      hand_cards = hash['hand'].map { |card| Card.load(card) } unless hash.nil?
+      self.new(hash['id'], hand_cards) unless hash.nil?
     end
   end
 end
