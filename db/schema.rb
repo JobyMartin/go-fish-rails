9a2f25145ff1d22a902556e277a6ba3c89c7ecf4
Joby Martin <jobymartin4th@gmail.com>
2026-07-15 15:40:41 -0400

    Add country column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_14_202314) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_193905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -146,6 +146,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_14_202314) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "country"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
