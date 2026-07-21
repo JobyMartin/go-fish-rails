282bc8124f441a54a7e159d014de530843ac90b0
Joby Martin <jobymartin4th@gmail.com>
2026-07-21 08:53:54 -0400

    Add Improvement 1 Deliverable A: STI subclass play_turn specs
    Characterize both branches of each game's play_turn dispatch:
    - GoFishGame: normal ask delegation + fish-and-skip on empty hand
    - CrazyEightsGame: place a given card + draw-until-playable

--- spec/models/crazy_eights_game_spec.rb
+++ spec/models/crazy_eights_game_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CrazyEightsGame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#play_turn' do
+    let(:game) { create(:game, type: 'CrazyEightsGame') }
+
+    before do
+      create(:player, game:)
+      create(:player, game:)
+      game.start
+    end
+
+    it 'places a given card onto William and advances the turn' do
+      state = game.game_state
+      before_index = state.current_player_index
+      card = state.current_player.hand.first
+      game.play_turn(state.william.active_card, "#{card.rank} #{card.suit}")
+      expect(state.william.active_card).to eq card
+      expect(state.current_player_index).not_to eq before_index
+    end
+
+    it 'draws until a playable card when none is given' do
+      state = game.game_state
+      active = state.william.active_card
+      game.play_turn(active)
+      placed = state.william.active_card
+      expect(placed.suit == active.suit || placed.rank == active.rank).to be true
+    end
+  end
 end
+
--- spec/models/go_fish_game_spec.rb
+++ spec/models/go_fish_game_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe GoFishGame, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#play_turn' do
+    let(:game) { create(:game, type: 'GoFishGame') }
+
+    before do
+      create(:player, game:)
+      create(:player, game:)
+      game.start
+    end
+
+    it 'delegates a normal ask to the domain game_state' do
+      state = game.game_state
+      opponent = state.players.last
+      expect { game.play_turn(opponent.id, 'A') }
+        .to change { state.round_results.size }.by(1)
+    end
+
+    it 'fishes and skips when the current player has an empty hand' do
+      state = game.game_state
+      fisher = state.current_player
+      fisher.hand = []
+      game.play_turn(0, 'A')
+      expect(fisher.hand_size).to eq 1
+    end
+  end
 end
